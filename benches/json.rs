91b2a7e6d16d87df94e86e715ed2859e273bd688
赵坤 <zhaokun@ziipin.com>
2022-11-01 22:12:26 +0800

    [feature] opt benches

--- benches/json.rs
+++ benches/json.rs
@@ -1,25 +1,17 @@
 use criterion::criterion_main;
 
 use criterion::{criterion_group, Criterion};
-use json::{JsonValue, ToJson};
-
-fn parse(str: &str) -> JsonValue {
-    json::parse(str).unwrap()
-}
-
-fn pretty(json: &JsonValue) {
-    let _ = json.pretty();
-}
+use json::ToJson;
 
 fn benchmark_parse(c: &mut Criterion) {
     let json = std::fs::read_to_string("./test.json").unwrap();
-    c.bench_function("parse", |b| b.iter(|| parse(&json)));
+    c.bench_function("parse", |b| b.iter(|| json::parse(&json)));
 }
 
 fn benchmark_pretty(c: &mut Criterion) {
     let json = std::fs::read_to_string("./test.json").unwrap();
-    let json = parse(&json);
-    c.bench_function("pretty", |b| b.iter(|| pretty(&json)));
+    let json = json::parse(&json).unwrap();
+    c.bench_function("pretty", |b| b.iter(|| json.pretty()));
 }
 
 criterion_group!(benches, benchmark_parse, benchmark_pretty);
